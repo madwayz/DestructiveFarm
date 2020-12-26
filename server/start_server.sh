4383fb324c3363351e50fba8b0b90bec22f23b0e
madwayz <djurball@list.ru>
2020-12-26 14:29:28 +0700

    resolve conflict

--- server/start_server.sh
+++ server/start_server.sh
@@ -2,8 +2,4 @@
 
 # Use FLASK_DEBUG=True if needed
 
-<<<<<<< HEAD
-FLASK_APP=__init__.py flask run --host 0.0.0.0 --with-threads
-=======
 FLASK_APP=standalone.py python3 -m flask run --host 0.0.0.0 --with-threads
->>>>>>> upstream/master
